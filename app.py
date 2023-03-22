a53d91edb8c5910cae7c823cbdf2ff2c18605e59
heewon Choi <wonbb3313@gmail.com>
2023-03-22 13:20:47 +0900

    feat/업로드 화면 크게  구성

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/mypage')
-def home2():
-   return 'mypage입니다'
 
 @app.route('/')
-def home():
+def home_page():
    return render_template('index.html')
+
+@app.route('/upload')
+def upload_page():
+   return render_template('upload.html')
+
+# 파일 업로드
+# @app.route('/file', methods=['POST'])
+# def file_upload():
+# 	file = request.files['file']
+    	
+#    filename = secure_filename(file.filename)
+#    os.makedirs(image_path, exists_ok=True)
+#    file.save(os.path.join(image_path, filename)
+      
+#    return
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
