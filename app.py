4a666af80159e5fa2e0f8a90628e95f7b93b1251
heewon Choi <wonbb3313@gmail.com>
2023-03-25 19:21:02 +0900

    feat/파일 업로드 페이지 완성

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
 
+# page
 @app.route('/')
 def home_page():
    return render_template('index.html')
+
+@app.route('/list')
+def list_page():
+   return render_template('list.html')
 
 @app.route('/upload')
 def upload_page():
    return render_template('upload.html')
 
-# 파일 업로드
-# @app.route('/file', methods=['POST'])
-# def file_upload():
-# 	file = request.files['file']
-    	
-#    filename = secure_filename(file.filename)
-#    os.makedirs(image_path, exists_ok=True)
-#    file.save(os.path.join(image_path, filename)
-      
-#    return
 
+
+# s3 connection
+# import boto3
+# def s3_connection():
+   
+
+
+# file uploading
+@app.route('/file_upload', methods=['POST'])
+def upload_image():
+   if request.method == "POST":
+      if request.files['file'].filename == '':
+          flash('파일이 없습니다. 파일을 제출하세요!') 
+          # 파일이 없으면 flash 전달. (현재 창에서 flash 메시지 출력.) 
+          return redirect(url_for('upload'))
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return '파일이 저장되었습니다'
+   else:
+      return render_template('upload.html')
+   
+   
+   
+   
+   
+	
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
+   app.secret_key = 'super secret key'
+   app.config['SESSION_TYPE'] = 'filesystem'
